57267a358222219d20246f16caaa51d5c0bdfd3b
Brian Pompey <brianthescholar@gmail.com>
2020-01-30 02:50:48 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -22,6 +22,7 @@ class SongsController < ApplicationController
     @song.genre_ids = params[:genres]
     @song.save
 
+    flash[:message] = "Successfully created song." 
     redirect("/songs/#{@song.slug}")
   end
 
