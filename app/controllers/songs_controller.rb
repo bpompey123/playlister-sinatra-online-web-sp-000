e755c70adeb97c656c8f565031fadaff6b5b6772
Brian Pompey <brianthescholar@gmail.com>
2020-01-30 02:20:33 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -19,7 +19,7 @@ class SongsController < ApplicationController
     @song.artist = Artist.find_or_create_by(name: params[:artist][:name])
     @song.genre_ids = params[:genres]
     @song.save
-    
+
     redirect("/songs/#{@song.slug}")
   end
 
